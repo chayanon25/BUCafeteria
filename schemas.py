9586c255dc8d42ca850194d1be88e11447e194cf
Nathapong Piromthong <nathapongpaulp@gmail.com>
2025-11-19 21:52:55 +0700

    schemas done

--- schemas.py
+++ schemas.py
@@ -1,3 +1,68 @@
 from pydantic import BaseModel
-from datetime import datetime
+from typing import Optional
 
+# Location
+class LocationBase(BaseModel):
+  name: str
+  description: Optional[str]
+
+class LocationCreate(LocationBase):
+  pass
+
+class LocationOut(LocationBase):
+  id: str
+
+  class Config:
+    orm_mode = True
+
+# Restaurant
+class RestaurantBase(BaseModel):
+  name: str
+  description: Optional[str]
+  image_url: Optional[str]
+
+class RestaurantCreate(RestaurantBase):
+  location_id: Optional[str] = None
+  new_location: Optional[LocationCreate] = None
+
+  opening_hours: Optional[str] = None
+  phone: Optional[str] = None
+
+class RestaurantOut(RestaurantBase):
+  id: str
+  location: LocationOut
+
+  class Config:
+    orm_mode = True
+
+#Category
+class CategoryBase(BaseModel):
+  name: str
+
+class CategoryCreate(CategoryBase):
+  pass
+
+class CategoryOut(CategoryBase):
+  id: str
+  class Config:
+    orm_mode = True
+
+#Menu
+class MenuBase(BaseModel):
+  name: str
+  price: float
+  description: Optional[str]
+  image_url: Optional[str]
+
+class MenuCreate(MenuBase):
+  restaurant_id: str
+  category_id: Optional[str] = None
+  new_category: Optional[CategoryCreate] = None
+
+class MenuOut(MenuBase):
+  id: str
+  restaurant_id: str
+  category: CategoryOut
+
+  class Config:
+    orm_mode = True
